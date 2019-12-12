958b6ab73a7fbea2eb7bc205b132a22a309bfacb
Curso <Curso@LSTKMY231256.stktrainning.com>
2019-12-11 21:12:31 -0600

    prueba

---
+++ Genericos/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Genericos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            MuestraNombre<Perro>(new Perro("panfilo"), new Perro("galleta"));
+            Console.ReadKey();
+        }
+
+
+        public static void MuestraNombre<Tipo>(params Tipo[] Objeto) where Tipo : ElementoConNombre
+        {
+            foreach (var o in Objeto)
+                Console.WriteLine("la mascota se llama " + o.RetornaNombre());
+            //Console.WriteLine("la mascota se llama " + Objeto[0].RetornaNombre() + "/n");
+            //Console.WriteLine("el mueble se llama " + Objeto[1].RetornaNombre() + "/n");
+        }
+    }
+    public interface ElementoConNombre
+    {
+        string RetornaNombre();
+    }
+
+    public class Mueble : ElementoConNombre
+    {
+
+        public string RetornaNombre()
+        {
+            return "Nombre del mueble";
+        }
+    }
+
+    public class Perro : ElementoConNombre
+    {
+        string Nombre;
+        public Perro(string _nombre)
+        {
+            Nombre = _nombre;
+        }
+        public string RetornaNombre()
+        {
+            return Nombre;
+        }
+    }
+
+    public class Gato : ElementoConNombre
+    {
+        string Nombre;
+        public Gato(string _nombre)
+        {
+            Nombre = _nombre;
+        }
+        public string RetornaNombre()
+        {
+            return Nombre;
+        }
+    }
+}
